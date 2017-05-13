6b1e8f1a7af6ea393ebf5560c914b298410e827e
RONALD SILVA <ronald.silva4@fatec.sp.gov.br>
2017-05-13 20:25:51 -0300

    class 04

--- TkinterBitcoin/main.py
+++ TkinterBitcoin/main.py
@@ -14,14 +14,18 @@ class SeaofBTCapp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky='nsew')
+
+        for F in (StartPage, PageOne, PageTwo):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
+
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
+
 
 class StartPage(tk.Frame):
 
@@ -29,6 +33,63 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text='Start Page', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(
+            self, 
+            text='Visit Page 1', 
+            command=lambda: controller.show_frame(PageOne)
+        )
+        button1.pack()
+        button2 = tk.Button(
+            self,
+            text='Visit Page 2',
+            command=lambda: controller.show_frame(PageTwo)
+        )
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='Page One', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(
+            self,
+            text='Back to Home',
+            command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+
+        button2 = tk.Button(
+            self,
+            text='Visit Page 2',
+            command=lambda: controller.show_frame(PageTwo)
+        )
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='Page Two', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(
+            self,
+            text='Back to Home',
+            command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+
+        button2 = tk.Button(
+            self,
+            text='Visit Page 1',
+            command=lambda: controller.show_frame(PageOne)
+        )
+        button2.pack()
 
 
 if __name__ == '__main__':
