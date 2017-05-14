9bb98ad6176bb7a377fe4a9ff72c46c47a2a3c50
RONALD SILVA <ronald.silva4@fatec.sp.gov.br>
2017-05-14 11:30:47 -0300

    class 07

--- TkinterBitcoin/main.py
+++ TkinterBitcoin/main.py
@@ -1,8 +1,36 @@
 import tkinter as tk
 from tkinter import ttk
 
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+from matplotlib import animation
+from matplotlib import style
+
 
 LARGE_FONT = ('Verdana', 12)
+style.use('ggplot')
+
+figure = Figure(figsize=(5, 5), dpi=100)
+subplot = figure.add_subplot(111)
+        
+
+def animate(i):
+    pull_data = open('sample_data.txt', 'r').read()
+    data_list = pull_data.split('\n')
+    x_list = []
+    y_list = []
+
+    for each_line in data_list:
+        if len(each_line) > 1:
+            x, y = each_line.split(',')
+            x_list.append(int(x))
+            y_list.append(int(y))
+
+    subplot.clear()
+    subplot.plot(x_list, y_list)
 
 
 class SeaofBTCapp(tk.Tk):
@@ -20,7 +48,7 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky='nsew')
@@ -51,6 +79,12 @@ class StartPage(tk.Frame):
             command=lambda: controller.show_frame(PageTwo)
         )
         button2.pack()
+        button3 = ttk.Button(
+            self,
+            text='Graph Page',
+            command=lambda: controller.show_frame(PageThree)
+        )
+        button3.pack()
 
 
 class PageOne(tk.Frame):
@@ -97,6 +131,30 @@ class PageTwo(tk.Frame):
         button2.pack()
 
 
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='Graph Page', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(
+            self,
+            text='Back to Home',
+            command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+
+        canvas = FigureCanvasTkAgg(figure, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
 if __name__ == '__main__':
     app = SeaofBTCapp()
+    anime = animation.FuncAnimation(figure, animate, interval=1000)
     app.mainloop()
